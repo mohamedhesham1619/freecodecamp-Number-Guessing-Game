6268f8fa604a64b5f3da62ac9b371ca0fca8045e
Mohamed Hesham <63409218+mohamedhesham1619@users.noreply.github.com>
2022-09-20 04:06:10 +0200

    Create number_guess.sql

---
+++ number_guess.sql
@@ -0,0 +1,81 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 12.9 (Ubuntu 12.9-2.pgdg20.04+1)
+-- Dumped by pg_dump version 12.9 (Ubuntu 12.9-2.pgdg20.04+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+DROP DATABASE number_guess;
+--
+-- Name: number_guess; Type: DATABASE; Schema: -; Owner: freecodecamp
+--
+
+CREATE DATABASE number_guess WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'C.UTF-8' LC_CTYPE = 'C.UTF-8';
+
+
+ALTER DATABASE number_guess OWNER TO freecodecamp;
+
+\connect number_guess
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: freecodecamp
+--
+
+CREATE TABLE public.users (
+    username character varying,
+    games_played integer,
+    best_game integer
+);
+
+
+ALTER TABLE public.users OWNER TO freecodecamp;
+
+--
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: freecodecamp
+--
+
+INSERT INTO public.users VALUES ('m', 1, NULL);
+INSERT INTO public.users VALUES ('user_1663638067875', 2, 587);
+INSERT INTO public.users VALUES ('user_1663638067876', 5, 55);
+INSERT INTO public.users VALUES ('user_1663637920270', 5, 20);
+INSERT INTO public.users VALUES ('user_1663637524730', 6, 3);
+INSERT INTO public.users VALUES ('user_1663638177679', 2, 294);
+INSERT INTO public.users VALUES ('user_1663638177680', 5, 88);
+INSERT INTO public.users VALUES ('user_1663638188434', 2, 424);
+INSERT INTO public.users VALUES ('user_1663638188435', 5, 214);
+INSERT INTO public.users VALUES ('user_1663637524729', 2, 267);
+INSERT INTO public.users VALUES ('user_1663637920269', 2, 455);
+INSERT INTO public.users VALUES ('user_1663636856624', 2, 43);
+INSERT INTO public.users VALUES ('user_1663636856625', 5, 64);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
